1e13499662dd610fc74ed90cf0b391dc48e75498
sujeet kumar <sujeetkr2508@gmail.com>
2023-05-25 11:04:54 +0530

    App.js created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentKey: '',
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleKeyPress = (e) => {
+    // Check if the pressed key matches the expected key
+    if (e.key === this.state.currentKey) {
+      // Update the state to trigger a re-render
+      this.setState({
+        currentKey: this.getRandomKey(),
+      });
+    }
+  };
+
+  getRandomKey = () => {
+    const keys = ['a', 's', 'd', 'f', 'j', 'k', 'l', ';'];
+    // Get a random key from the array
+    return keys[Math.floor(Math.random() * keys.length)];
+  };
+
+  componentDidMount() {
+    // Add event listener to handle key presses
+    document.addEventListener('keydown', this.handleKeyPress);
+    // Set the initial random key
+    this.setState({
+      currentKey: this.getRandomKey(),
+    });
+  }
+
+  componentWillUnmount() {
+    // Remove event listener to clean up
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  render() {
+    return (
+      <div className="cont">
+        <h1>Touch Typing Exercise</h1>
+        <p>Press the current key: {this.state.currentKey}</p>
+        <div className="keyboard">
+          {['a', 's', 'd', 'f', 'j', 'k', 'l', ';'].map((key) => (
+            <div key={key} className="key">
+              {key}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
 }
-
 export default App;
